f2f7f562f45559e99e0d82e65effeb5515f06620
unknown <paulabelmiro@yahoo.com.br>
2025-01-10 10:53:55 -0300

    feat: Melhorias na página do curso para acrescentar metadata dinâmico.

--- evob/src/app/courses/[slug]/page.tsx
+++ evob/src/app/courses/[slug]/page.tsx
@@ -1,23 +1,59 @@
+import type { Metadata, ResolvingMetadata } from "next";
 import CourseDetails from "@/components/CourseDetails";
 import ShareButton from "@/components/ShareButton";
 import { getCourseDetails } from "@/services/getCourseDetails";
 import { notFound } from "next/navigation";
 
-export default async function Course({ params }: {
-  params: Promise<{ slug: string }>;
-}) {
-  const { slug } = await params;
-  try {
-    const course = await getCourseDetails(slug);
+type CourseProps = {
+  params: Promise<{ slug: string }>
+}
 
-    return (
-      <>
-        <CourseDetails course={course} />
-        <ShareButton />
-      </>
-    )
-  } catch (error) {
-    console.log(error);
-    return notFound();
+// Variável para armazenar os dados do curso em cache durante a execução.
+const courseCache = new Map<string, Awaited<ReturnType<typeof getCourseDetails>>>();
+
+// Gera metadata dinâmico para a página do curso
+export async function generateMetadata(
+  { params }: CourseProps,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const slug = (await params).slug;
+
+  // Busca os detalhes do curso, armazenando no cache
+  if (!courseCache.has(slug)) {
+    const course = await getCourseDetails(slug);
+    courseCache.set(slug, course);
+  }
+
+  const course = courseCache.get(slug)!;
+  const previousImages = (await parent).openGraph?.images || []
+
+  return {
+    title: course.title,
+    openGraph: {
+      images: [course.thumbnail, ...previousImages],
+    },
   }
 }
+
+export default async function Course({ params }: CourseProps) {
+  const slug = (await params).slug;
+
+  // Recupera o curso do cache
+  let course = courseCache.get(slug);
+  if (!course) {
+    try {
+      course = await getCourseDetails(slug);
+    } catch (error) {
+      console.log(error);
+      // Se não encontrar o curso retorna a página 404
+      return notFound();
+    }
+  }
+
+  return (
+    <>
+      <CourseDetails course={course} />
+      <ShareButton />
+    </>
+  )
+} 
