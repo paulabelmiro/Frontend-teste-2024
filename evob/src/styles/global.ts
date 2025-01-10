0c22b90ed5163ba51bc72fb415b5d24e7d0e265c
unknown <paulabelmiro@yahoo.com.br>
2025-01-10 15:27:35 -0300

    style: Personalização a barra de rolagem.

--- evob/src/styles/global.ts
+++ evob/src/styles/global.ts
@@ -5,6 +5,10 @@ export const GlobalStyle = createGlobalStyle`
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+
+        scrollbar-width: thin;
+        scrollbar-color: ${props => props.theme.purple} transparent;
+        scrollbar-arrow-color: ${props => props.theme.purple};
     }
 
     body {
@@ -13,5 +17,19 @@ export const GlobalStyle = createGlobalStyle`
 
     button {
         cursor: pointer;
+    }
+
+    *::-webkit-scrollbar {
+        width: 3px;
+        height: 3px;
+    }
+
+    *::-webkit-scrollbar-track {
+        background: transparent;
+        padding: 2px;
+    }
+
+    *::-webkit-scrollbar-thumb {
+        background-color: ${props => props.theme.purple};
     }
 `
