1fefbdbf487acefcb297b161ea2c1801230e219d
unknown <paulabelmiro@yahoo.com.br>
2025-01-12 10:31:58 -0300

    style: Alteração do botão de compartinhar de posição absoluta para fixo.

--- evob/src/components/ShareButton/styles.ts
+++ evob/src/components/ShareButton/styles.ts
@@ -6,11 +6,11 @@ export const ShareButtonContainer = styled.button`
     align-items: center;
     padding: 16px;
     gap: 10px;
-    position: absolute;
+    position: fixed;
     width: 56px;
     height: 56px;
-    left: 75%;//290px;
-    top: 459px;
+    left: 75%;
+    top: 60vh;
     background: ${props => props.theme.white};
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1);
     border-radius: 99999997952px;
@@ -18,6 +18,6 @@ export const ShareButtonContainer = styled.button`
 
     @media screen and (min-width: 1024px) {
         left: 91%;
-        top: 770px;
+        top: 75vh;
     }
 `
